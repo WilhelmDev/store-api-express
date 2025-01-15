7549f495467a3966ef6cf5915722ff5b1b97564a
Wilhelm Flores <wiljoseflores@gmail.com>
2025-01-15 18:51:57 -0400

    feat: adjust for docker

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { logServerStart, logRouteAccess } from './utils/logger';
 import routes from './routes';
 
@@ -7,9 +8,15 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
+app.use(cors())
+
 // Middleware para loggear accesos a rutas
 app.use((req, res, next) => {
   logRouteAccess(req.method, req.path, req.ip || 'unknown');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+    return;
+  }
   next();
 });
 
